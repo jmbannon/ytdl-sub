111e15c29a5a39669c8cbba6e36d6a21dfd397c7
jbannon <jbann1994@gmail.com>
2022-07-04 05:44:39 +0000

    readability

--- tests/e2e/expected_transaction_log.py
+++ tests/e2e/expected_transaction_log.py
@@ -18,30 +18,29 @@ def assert_transaction_log_matches(
         Output directory the files are saved to
     transaction_log
         Transaction log to check
-    expected_transaction_log_summary
+    transaction_log_summary_file_name
         Name if the transaction log summary to compare.
         Lives in tests/e2e/resources/transaction_log_summaries
     """
-
+    # Read the expected summary file
     with open(
         _TRANSACTION_LOG_SUMMARY_PATH / transaction_log_summary_file_name, "r", encoding="utf-8"
     ) as summary_file:
-        expected_summary = summary_file.read()
-    expected_summary = expected_summary.format(output_directory=output_directory)
-    summary = transaction_log.to_output_message(output_directory=output_directory)
+        summary = transaction_log.to_output_message(output_directory=output_directory)
+        expected_summary = summary_file.read().format(output_directory=output_directory)
 
-    # Ensure there are the same number of new lines
+    # Split, ensure there are the same number of new lines
     summary_lines: List[str] = summary.split("\n")
     expected_summary_lines: List[str] = expected_summary.split("\n")
-
     assert len(summary_lines) == len(
         expected_summary_lines
     ), f"Summary number of lines differ: {len(summary_lines) != len(expected_summary_lines)}"
 
+    # Ensure each line equals
     for idx in range(len(summary_lines)):
         line = summary_lines[idx]
         expected_line = expected_summary_lines[idx]
         assert (
             summary_lines[idx] == expected_summary_lines[idx],
-            f"Summary line {idx} differs: '{line}' != {expected_line}",
+            f"Summary line {idx} differs: '{line}' != '{expected_line}'",
         )
