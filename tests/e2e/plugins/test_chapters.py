61c5a22933b7a1326a30a2dac937ef42b7a2e458
Jesse Bannon <jbann1994@gmail.com>
2022-08-26 23:08:53 -0700

    [REFACTOR] Move chapter timestamps support into chapter plugin, remove from youtube.video (#193)
    Introduces breaking changes to youtube.video.chapter_timestamps. Please use chapters.embed_chapter_timestamps instead to embed timestamps into a video

--- tests/e2e/plugins/test_chapters.py
+++ tests/e2e/plugins/test_chapters.py
@@ -71,3 +71,42 @@ class TestChapters:
                 "JMC - This GPU SLIDES into this Case! - Silverstone SUGO 16 ITX Case.mp4"
             ],
         )
+
+    @pytest.mark.parametrize("dry_run", [True, False])
+    def test_chapters_from_timestamp_file_with_subs(
+        self,
+        music_video_config,
+        single_video_sponsorblock_and_embedded_subs_preset_dict,
+        timestamps_file_path,
+        output_directory,
+        dry_run,
+    ):
+        # Test chapters and video tags, throw in a video tag with special chars while we are at it
+        single_video_sponsorblock_and_embedded_subs_preset_dict["chapters"] = {
+            "embed_chapters": False,
+            "embed_chapter_timestamps": timestamps_file_path,
+        }
+        single_video_sponsorblock_and_embedded_subs_preset_dict["video_tags"] = {
+            "tags": {"description": "🎸 / ' \" \n newline?"}
+        }
+
+        subscription = Subscription.from_dict(
+            config=music_video_config,
+            preset_name="chapters_from_timestamps_with_subs",
+            preset_dict=single_video_sponsorblock_and_embedded_subs_preset_dict,
+        )
+
+        transaction_log = subscription.download(dry_run=dry_run)
+        assert_transaction_log_matches(
+            output_directory=output_directory,
+            transaction_log=transaction_log,
+            transaction_log_summary_file_name="plugins/test_chapters_from_ts_with_subs.txt",
+        )
+        assert_expected_downloads(
+            output_directory=output_directory,
+            dry_run=dry_run,
+            expected_download_summary_file_name="plugins/test_chapters_from_ts_with_subs.json",
+            ignore_md5_hashes_for=[
+                "JMC - This GPU SLIDES into this Case! - Silverstone SUGO 16 ITX Case.mp4"
+            ],
+        )
--- tests/e2e/youtube/test_video.py
+++ tests/e2e/youtube/test_video.py
@@ -86,34 +86,3 @@ class TestYoutubeVideo:
             dry_run=dry_run,
             expected_download_summary_file_name="youtube/test_video.json",
         )
-
-    @pytest.mark.parametrize("dry_run", [True, False])
-    def test_single_video_with_timestamp_chapters_download(
-        self,
-        timestamps_file_path,
-        music_video_config,
-        single_video_preset_dict,
-        output_directory,
-        dry_run,
-    ):
-        # Test chapters and video tags, throw in a video tag with special chars while we are at it
-        single_video_preset_dict["youtube"]["chapter_timestamps"] = timestamps_file_path
-        single_video_preset_dict["video_tags"]["tags"]["description"] = "🎸 / ' \" \n newline?"
-
-        single_video_subscription = Subscription.from_dict(
-            config=music_video_config,
-            preset_name="music_video_single_video_test",
-            preset_dict=single_video_preset_dict,
-        )
-
-        transaction_log = single_video_subscription.download(dry_run=dry_run)
-        assert_transaction_log_matches(
-            output_directory=output_directory,
-            transaction_log=transaction_log,
-            transaction_log_summary_file_name="youtube/test_video_with_chapter_timestamps.txt",
-        )
-        assert_expected_downloads(
-            output_directory=output_directory,
-            dry_run=dry_run,
-            expected_download_summary_file_name="youtube/test_video_with_chapter_timestamps.json",
-        )
