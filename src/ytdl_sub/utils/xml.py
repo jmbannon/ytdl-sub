389e6313e3b5f26db946c859563fe4732a02a7cb
Jesse Bannon <jbann1994@gmail.com>
2022-08-26 23:42:51 -0700

    set text

--- src/ytdl_sub/utils/xml.py
+++ src/ytdl_sub/utils/xml.py
@@ -1,6 +1,5 @@
-from typing import Dict
-
 import xml.etree.ElementTree as et
+from typing import Dict
 
 
 def _to_max_3_byte_utf8_char(char: str) -> str:
@@ -55,7 +54,7 @@ def to_xml(nfo_dict: Dict[str, str], nfo_root: str) -> bytes:
     """
     xml_root = et.Element(nfo_root)
     for key, value in nfo_dict.items():
-        et.SubElement(xml_root, value)
+        sub_element = et.SubElement(xml_root, key)
+        sub_element.text = value
 
-    return et.tostring(element=xml_root, encoding='utf-8', xml_declaration=True)
-
+    return et.tostring(element=xml_root, encoding="utf-8", xml_declaration=True)
