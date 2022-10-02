fed25b95348995a2d6cec8e860c53af091aa5f9b
Jesse Bannon <jbann1994@gmail.com>
2022-10-01 21:37:52 -0700

    revert channel test

--- tests/e2e/youtube/test_channel.py
+++ tests/e2e/youtube/test_channel.py
@@ -8,10 +8,10 @@ from ytdl_sub.subscriptions.subscription import Subscription
 @pytest.fixture
 def channel_preset_dict(output_directory):
     return {
-        "preset": [
-            "jellyfin_tv_show_url",
-            "season_by_year__episode_by_month_day",
-        ],
+        "preset": "yt_channel_as_tv",
+        "youtube": {"channel_url": "https://youtube.com/channel/UCcRSMoQqXc_JrBZRHDFGbqA"},
+        # override the output directory with our fixture-generated dir
+        "output_options": {"output_directory": output_directory},
         # download the worst format so it is fast
         "ytdl_options": {
             "format": "worst[ext=mp4]",
@@ -19,7 +19,7 @@ def channel_preset_dict(output_directory):
             "break_on_reject": False,  # do not break from max views
         },
         "subtitles": {
-            "subtitles_name": "{episode_file_path}.{lang}.{subtitles_ext}",
+            "subtitles_name": "{episode_name}.{lang}.{subtitles_ext}",
             "allow_auto_generated_subtitles": True,
         },
         "nfo_tags": {
@@ -33,11 +33,7 @@ def channel_preset_dict(output_directory):
                 "source_uploader": "{playlist_uploader}",
             }
         },
-        "overrides": {
-            "url": "https://youtube.com/channel/UCcRSMoQqXc_JrBZRHDFGbqA",
-            "tv_show_name": "Project / Zombie",
-            "tv_show_directory": output_directory,
-        },
+        "overrides": {"tv_show_name": "Project / Zombie"},
     }
 
 
