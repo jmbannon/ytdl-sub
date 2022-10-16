61bc390a9f89fbeea83d707c995ed83b91e65b68
Jesse Bannon <jbann1994@gmail.com>
2022-10-15 22:48:09 -0700

    [FEATURE] Add subscription yaml file preset `__preset__` and apply to all presets in the file

---
+++ tests/unit/config/test_subscription.py
@@ -0,0 +1,48 @@
+from typing import Dict
+from unittest.mock import patch
+
+import pytest
+
+from ytdl_sub.config.config_file import ConfigFile
+from ytdl_sub.plugins.nfo_tags import NfoTagsOptions
+from ytdl_sub.subscriptions.subscription import Subscription
+
+
+@pytest.fixture
+def preset_file(youtube_video: Dict, output_options: Dict) -> Dict:
+    return {
+        "__preset__": {
+            "preset": "parent_preset_3",
+            "youtube": youtube_video,
+            "output_options": output_options,
+            "nfo_tags": {
+                "tags": {"key-3": "file_preset"},
+            },
+        },
+        "test_preset": {
+            "nfo_tags": {
+                "tags": {"key-4": "test_preset"},
+            }
+        },
+    }
+
+
+def test_subscription_file_preset_applies(config_file: ConfigFile, preset_file: Dict):
+    with patch("ytdl_sub.subscriptions.subscription.load_yaml") as mock_load_yaml:
+        mock_load_yaml.return_value = preset_file
+
+        subs = Subscription.from_file_path(config=config_file, subscription_path="mocked")
+        assert len(subs) == 1
+
+        nfo_options: NfoTagsOptions = subs[0].plugins.get(NfoTagsOptions)
+        tags_string_dict = {
+            key: formatter[0].format_string
+            for key, formatter in nfo_options.tags.string_tags.items()
+        }
+
+        assert tags_string_dict == {
+            "key-1": "preset_0",
+            "key-2": "preset_2",
+            "key-3": "file_preset",
+            "key-4": "test_preset",
+        }
