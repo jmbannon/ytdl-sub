02a9fa88e93480b1c19ab084e3e9ea0268ae58c6
Jesse Bannon <jbann1994@gmail.com>
2023-02-20 10:53:01 -0800

    [DOCS] Automatic sanitized override variable docs

--- src/ytdl_sub/config/preset_options.py
+++ src/ytdl_sub/config/preset_options.py
@@ -114,6 +114,10 @@ class Overrides(DictFormatterValidator):
 
     Override variables can contain explicit values and other variables, including both override
     and source variables.
+
+    In addition, any override variable defined will automatically create a `sanitized` version.
+    In the example above, `output_directory_sanitized` will exist and perform sanitization on
+    the value.
     """
 
     # pylint: enable=line-too-long
