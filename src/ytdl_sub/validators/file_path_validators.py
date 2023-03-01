eafa0a7c68409985dc3fcb7a3b1ba2dfeaa0b21b
Jesse Bannon <jbann1994@gmail.com>
2023-02-28 16:32:32 -0800

    Path

--- src/ytdl_sub/validators/file_path_validators.py
+++ src/ytdl_sub/validators/file_path_validators.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from typing import Dict
 
 from ytdl_sub.validators.string_formatter_validators import StringFormatterValidator, OverridesStringFormatterValidator
@@ -8,11 +9,11 @@ class StringFormatterFilePathValidator(StringFormatterValidator):
     _expected_value_type_name = "filepath"
 
     def apply_formatter(self, variable_dict: Dict[str, str]) -> str:
-        return os.path.relpath(super().apply_formatter(variable_dict))
+        return str(Path(super().apply_formatter(variable_dict)))
 
 
 class OverridesStringFormatterValidatorFilePathValidator(OverridesStringFormatterValidator):
     _expected_value_type_name = "static filepath"
 
     def apply_formatter(self, variable_dict: Dict[str, str]) -> str:
-        return os.path.relpath(super().apply_formatter(variable_dict))
+        return os.path.realpath(super().apply_formatter(variable_dict))
