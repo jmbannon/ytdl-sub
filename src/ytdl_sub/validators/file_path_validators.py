0054281317dc6aec15ec7d2cf203f65869e367ee
Jesse Bannon <jbann1994@gmail.com>
2023-03-01 09:14:52 -0800

    [DOCS] Add ffmpeg path docs

--- src/ytdl_sub/validators/file_path_validators.py
+++ src/ytdl_sub/validators/file_path_validators.py
@@ -1,10 +1,26 @@
 import os
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Any
 
 from ytdl_sub.validators.string_formatter_validators import OverridesStringFormatterValidator
 from ytdl_sub.validators.string_formatter_validators import StringFormatterValidator
+from ytdl_sub.validators.validators import StringValidator
 
+
+class ExistingFileValidator(StringValidator):
+    _expected_value_type = "file"
+
+    def __init__(self, name: str, value: Any):
+        super().__init__(name, value)
+        if not os.path.isfile(self._value):
+            raise self._validation_exception(
+                f"Expects an existing file, but '{self.value}' is not a file"
+            )
+
+    @property
+    def value(self) -> str:
+        """Turn into a Path, then a string, to get correct directory separators"""
+        return str(Path(self._value))
 
 class StringFormatterFilePathValidator(StringFormatterValidator):
     _expected_value_type_name = "filepath"
