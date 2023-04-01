14c78651655bb176a610ebe42b97e5c9d7c8db3a
Benjamin Hsieh <tanookiben@users.noreply.github.com>
2023-03-31 21:25:13 -0700

    [FEATURE] `channel_id` source variable (#575)

--- tests/unit/entries/conftest.py
+++ tests/unit/entries/conftest.py
@@ -73,6 +73,7 @@ def mock_entry_to_dict(
         "creator_sanitized": "abc123",
         "channel": "abc123",
         "channel_sanitized": "abc123",
+        "channel_id": uid,
         "extractor": extractor,
         "uploader": "abc123",
         "uploader_id": "abc123",
