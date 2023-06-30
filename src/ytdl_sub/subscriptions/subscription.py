5a7b0cd7f19c160d07df3ad54c68f2487a3c0345
Jesse Bannon <jbann1994@gmail.com>
2023-06-29 23:25:57 -0700

    [FEATURE] Add __value__ to subscription files to create one-liner subs

--- src/ytdl_sub/subscriptions/subscription.py
+++ src/ytdl_sub/subscriptions/subscription.py
@@ -5,10 +5,12 @@ from typing import List
 from ytdl_sub.config.config_file import ConfigFile
 from ytdl_sub.config.preset import Preset
 from ytdl_sub.subscriptions.subscription_download import SubscriptionDownload
+from ytdl_sub.utils.exceptions import ValidationException
 from ytdl_sub.utils.yaml import load_yaml
 from ytdl_sub.validators.validators import LiteralDictValidator
 
 FILE_PRESET_APPLY_KEY = "__preset__"
+FILE_SUBSCRIPTION_VALUE_KEY = "__value__"
 
 
 class Subscription(SubscriptionDownload):
@@ -64,7 +66,9 @@ class Subscription(SubscriptionDownload):
     @classmethod
     def from_file_path(cls, config: ConfigFile, subscription_path: str) -> List["Subscription"]:
         """
-        Loads subscriptions from a file and applies __preset__ to all of them if present.
+        Loads subscriptions from a file and applies ``__preset__`` to all of them if present.
+        If a subscription is in the form of key: value, it will set value to the override
+        variable defined in ``__value__``.
 
         Parameters
         ----------
@@ -76,11 +80,17 @@ class Subscription(SubscriptionDownload):
         Returns
         -------
         List of subscriptions, for each one in the subscription yaml
+
+        Raises
+        ------
+        ValidationException
+            If subscription file is misconfigured
         """
         subscriptions: List["Subscription"] = []
         subscription_dict = load_yaml(file_path=subscription_path)
 
         has_file_preset = FILE_PRESET_APPLY_KEY in subscription_dict
+        has_file_subscription_value = FILE_SUBSCRIPTION_VALUE_KEY in subscription_dict
 
         # If a file preset is present...
         if has_file_preset:
@@ -94,11 +104,38 @@ class Subscription(SubscriptionDownload):
             config = copy.deepcopy(config)
             config.presets.dict[FILE_PRESET_APPLY_KEY] = file_preset.dict
 
+        if has_file_subscription_value:
+            if not isinstance(subscription_dict[FILE_SUBSCRIPTION_VALUE_KEY], str):
+                raise ValidationException(
+                    f"Using {FILE_SUBSCRIPTION_VALUE_KEY} in a subscription"
+                    f"must be a string that corresponds to an override variable"
+                )
+
         for subscription_key, subscription_object in subscription_dict.items():
 
-            # Skip file preset
-            if subscription_key == FILE_PRESET_APPLY_KEY:
+            # Skip file preset or value
+            if subscription_key in [FILE_PRESET_APPLY_KEY, FILE_SUBSCRIPTION_VALUE_KEY]:
                 continue
+
+            # If the subscription obj is just a string, set it to the override variable
+            # defined in FILE_SUBSCRIPTION_VALUE_KEY
+            if isinstance(subscription_object, str) and has_file_subscription_value:
+                subscription_object = {
+                    "overrides": {
+                        subscription_dict[FILE_SUBSCRIPTION_VALUE_KEY]: subscription_object
+                    }
+                }
+            elif isinstance(subscription_object, dict):
+                pass
+            elif isinstance(subscription_object, str) and not has_file_subscription_value:
+                raise ValidationException(
+                    f"Subscription {subscription_key} is a string, but "
+                    f"{FILE_SUBSCRIPTION_VALUE_KEY} is not set to an override variable"
+                )
+            else:
+                raise ValidationException(
+                    f"Subscription {subscription_key} should be in the form of a preset"
+                )
 
             # If it has file_preset, inject it as a parent preset
             if has_file_preset:
