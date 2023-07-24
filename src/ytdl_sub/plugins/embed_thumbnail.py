c2cb5087337b9d78bdb967b98f3cfb10547976da
Jesse Bannon <jbann1994@gmail.com>
2023-07-24 10:11:39 -0700

    tests

--- src/ytdl_sub/plugins/embed_thumbnail.py
+++ src/ytdl_sub/plugins/embed_thumbnail.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+import mediafile
+
 from ytdl_sub.config.preset_options import OptionsValidator
 from ytdl_sub.entries.entry import Entry
 from ytdl_sub.plugins.plugin import Plugin
@@ -8,6 +10,7 @@ from ytdl_sub.utils.file_handler import FileHandler
 from ytdl_sub.utils.file_handler import FileMetadata
 from ytdl_sub.utils.logger import Logger
 from ytdl_sub.utils.thumbnail import convert_download_thumbnail
+from ytdl_sub.validators.audo_codec_validator import AUDIO_CODEC_EXTS
 from ytdl_sub.validators.validators import BoolValidator
 
 logger = Logger.get("embed_thumbnail")
@@ -34,6 +37,45 @@ class EmbedThumbnailPlugin(Plugin[EmbedThumbnailOptions]):
     def _embed_thumbnail(self) -> bool:
         return self.plugin_options.value
 
+    @classmethod
+    def _embed_video_thumbnail(cls, entry: Entry) -> None:
+        file_path = entry.get_download_file_path()
+        thumbnail_path = entry.get_download_thumbnail_path()
+        tmp_file_path = FFMPEG.tmp_file_path(file_path)
+        try:
+            cmd = [
+                "-i",
+                file_path,
+                "-i",
+                thumbnail_path,
+                "-map",
+                "1",
+                "-map",
+                "0",
+                "-dn",  # ignore data streams
+                "-c",
+                "copy",
+                "-bitexact",  # for reproducibility
+                "-disposition:0",
+                "attached_pic",
+                tmp_file_path,
+            ]
+            FFMPEG.run(cmd)
+            FileHandler.move(tmp_file_path, file_path)
+        finally:
+            FileHandler.delete(tmp_file_path)
+
+    @classmethod
+    def _embed_audio_file(cls, entry: Entry) -> None:
+        audio_file = mediafile.MediaFile(entry.get_download_file_path())
+        with open(entry.get_download_thumbnail_path(), "rb") as thumb:
+            mediafile_img = mediafile.Image(
+                data=thumb.read(), desc="cover", type=mediafile.ImageType.front
+            )
+
+        audio_file.images = [mediafile_img]
+        audio_file.save()
+
     def post_process_entry(self, entry: Entry) -> Optional[FileMetadata]:
         """
         Maybe embed the thumbnail
@@ -49,33 +91,9 @@ class EmbedThumbnailPlugin(Plugin[EmbedThumbnailOptions]):
             # convert the entry thumbnail so it is embedded as jpg
             convert_download_thumbnail(entry=entry)
 
-            file_path = entry.get_download_file_path()
-            thumbnail_path = entry.get_download_thumbnail_path()
-            tmp_file_path = FFMPEG.tmp_file_path(file_path)
-            try:
-                FFMPEG.run(
-                    [
-                        "-i",
-                        file_path,
-                        "-i",
-                        thumbnail_path,
-                        "-map",
-                        "0",
-                        "-map",
-                        "1",
-                        "-dn",  # ignore data streams
-                        "-c",
-                        "-copy",
-                        "-c:v:1",
-                        entry.thumbnail_ext,
-                        "-disposition:v:1",
-                        "attached_pic",
-                        "-bitexact",  # for reproducibility
-                        tmp_file_path,
-                    ]
-                )
-                FileHandler.move(tmp_file_path, file_path)
-            finally:
-                FileHandler.delete(tmp_file_path)
+            if entry.ext in AUDIO_CODEC_EXTS:
+                self._embed_audio_file(entry)
+            else:
+                self._embed_video_thumbnail(entry)
 
         return FileMetadata("Embedded thumbnail")
