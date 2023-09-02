aea59ea0cbdd166e1ee5516624af6e3fae6ff747
Jesse Bannon <jbann1994@gmail.com>
2023-09-01 23:44:54 -0700

    sadf

--- tests/resources.py
+++ tests/resources.py
@@ -1,7 +1,7 @@
 import shutil
 from pathlib import Path
 
-REGENERATE_FIXTURES: bool = True
+REGENERATE_FIXTURES: bool = False
 
 RESOURCE_PATH: Path = Path("tests") / "resources"
 _FILE_FIXTURE_PATH: Path = RESOURCE_PATH / "file_fixtures"
