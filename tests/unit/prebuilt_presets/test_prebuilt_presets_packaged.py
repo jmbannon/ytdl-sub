2d47c75a41499588e7782605862092d8117b3a58
Jesse Bannon <jbann1994@gmail.com>
2023-11-02 11:36:03 -0700

    [DEV] Test prebuilt presets packaged

---
+++ tests/unit/prebuilt_presets/test_prebuilt_presets_packaged.py
@@ -0,0 +1,15 @@
+class TestPrebuiltPresetsPackaged:
+    def test_helpers_packaged(self) -> None:
+        import ytdl_sub.prebuilt_presets.helpers
+
+    def test_internal_packaged(self) -> None:
+        import ytdl_sub.prebuilt_presets.internal
+
+    def test_music_packaged(self) -> None:
+        import ytdl_sub.prebuilt_presets.music
+
+    def test_music_videos_packaged(self) -> None:
+        import ytdl_sub.prebuilt_presets.music_videos
+
+    def test_tv_show_packaged(self) -> None:
+        import ytdl_sub.prebuilt_presets.tv_show
