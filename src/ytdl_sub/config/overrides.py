c0e2ded1ae11bafe42d361c93324903bb212761d
Jesse Bannon <jbann1994@gmail.com>
2023-12-19 00:22:14 -0800

    [BACKEND] Improved error messages when script resolution does not work (#846)

--- src/ytdl_sub/config/overrides.py
+++ src/ytdl_sub/config/overrides.py
@@ -11,7 +11,9 @@ from ytdl_sub.entries.variables.override_variables import SUBSCRIPTION_NAME
 from ytdl_sub.entries.variables.override_variables import OverrideVariables
 from ytdl_sub.script.parser import parse
 from ytdl_sub.script.script import Script
+from ytdl_sub.script.utils.exceptions import ScriptVariableNotResolved
 from ytdl_sub.utils.exceptions import InvalidVariableNameException
+from ytdl_sub.utils.exceptions import StringFormattingException
 from ytdl_sub.utils.exceptions import ValidationException
 from ytdl_sub.utils.script import ScriptUtils
 from ytdl_sub.utils.scriptable import Scriptable
@@ -170,6 +172,11 @@ class Overrides(DictFormatterValidator, Scriptable):
         Returns
         -------
         The format_string after .format has been called
+
+        Raises
+        ------
+        StringFormattingException
+            If the formatter that is trying to be resolved cannot
         """
         script: Script = self.script
         unresolvable: Set[str] = self.unresolvable
@@ -177,11 +184,19 @@ class Overrides(DictFormatterValidator, Scriptable):
             script = entry.script
             unresolvable = entry.unresolvable
 
-        return formatter.post_process(
-            str(
-                script.resolve_once(
-                    dict({"tmp_var": formatter.format_string}, **(function_overrides or {})),
-                    unresolvable=unresolvable,
-                )["tmp_var"]
+        try:
+            return formatter.post_process(
+                str(
+                    script.resolve_once(
+                        dict({"tmp_var": formatter.format_string}, **(function_overrides or {})),
+                        unresolvable=unresolvable,
+                    )["tmp_var"]
+                )
             )
-        )
+        except ScriptVariableNotResolved as exc:
+            raise StringFormattingException(
+                "Tried to resolve the following script, but could not due to unresolved "
+                f"variables:\n {formatter.format_string}\n"
+                "This is most likely due to circular dependencies in variables. "
+                "If you think otherwise, please file a bug on GitHub and post your config. Thanks!"
+            ) from exc
