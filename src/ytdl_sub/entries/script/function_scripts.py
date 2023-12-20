32e0a13ce5e2d10792f8fc6e2729c2b4ba2b51f6
Jesse Bannon <jbann1994@gmail.com>
2023-12-19 22:35:50 -0800

    simplify extract_field_from_siblings

--- src/ytdl_sub/entries/script/function_scripts.py
+++ src/ytdl_sub/entries/script/function_scripts.py
@@ -8,31 +8,14 @@ v: VariableDefinitions = VARIABLES
 CUSTOM_FUNCTION_SCRIPTS: Dict[str, str] = {
     #############################################################################################
     # SIBLING GETTER
-    "%extract_field_from_metadata_array_getter": """{
-        %map_get( %map(%array_at($0, 0)), %array_at($0, 1) )
-    }""",
-    "%extract_field_from_metadata_array": """{
-        %if(
-            %bool($0),
-            %array_extend(
-                %array_apply(
-                    %array_product(
-                        %array($0),
-                        [ %string($1) ]
-                    ),
-                    %extract_field_from_metadata_array_getter
-                )
-            ),
-            []
-        )   
-    }""",
     "%extract_field_from_siblings": f"""{{
         %if(
             %bool({v.sibling_metadata.variable_name}),
-            %extract_field_from_metadata_array(
+            %array_apply_fixed(
                 {v.sibling_metadata.variable_name},
-                $0
-            ),
+                %string($0),
+                %map_get
+            )
             []
         )
     }}""",
