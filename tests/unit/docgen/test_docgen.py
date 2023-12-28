8b2b85e54d78e88fbdf0abcd68bb2ec30cf62db7
Jesse Bannon <jbann1994@gmail.com>
2023-12-28 15:42:13 -0800

    hmm

--- tests/unit/docgen/test_docgen.py
+++ tests/unit/docgen/test_docgen.py
@@ -5,21 +5,13 @@ from tools.docgen.entry_variables import EntryVariablesDocGen
 from tools.docgen.override_variables import OverrideVariablesDocGen
 from tools.docgen.plugins import PluginsDocGen
 from tools.docgen.scripting_functions import ScriptingFunctionsDocGen
-from ytdl_sub.utils.file_handler import get_file_md5_hash
 from ytdl_sub.utils.file_handler import get_md5_hash
 
 
 def _test_doc_gen(doc_gen: Type[DocGen]) -> None:
     expected_md5_hash = get_md5_hash(doc_gen.generate_and_maybe_write_to_file())
-    md5_hash = get_file_md5_hash(doc_gen.LOCATION)
-
-    if md5_hash != expected_md5_hash:
-        print("generated")
-        print(doc_gen.generate())
-
-        print("expected")
-        with open(doc_gen.LOCATION, "r", encoding="utf-8") as file_doc:
-            print(file_doc.read())
+    with open(doc_gen.LOCATION, "r", encoding="utf-8") as file_doc:
+        md5_hash = get_md5_hash(file_doc.read())
 
     assert md5_hash == expected_md5_hash
 
