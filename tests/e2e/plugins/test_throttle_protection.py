82429bd87da22ed9c3794b6c55fb625adda61066
Jesse Bannon <jbann1994@gmail.com>
2024-09-26 00:14:30 -0700

    [DEV] Partially unit test throttle protection

--- tests/e2e/plugins/test_throttle_protection.py
+++ tests/e2e/plugins/test_throttle_protection.py
@@ -1,22 +1,6 @@
 import pytest
-from conftest import assert_logs
 
-from ytdl_sub.plugins.throttle_protection import logger as throttle_protection_logger
 from ytdl_sub.subscriptions.subscription import Subscription
-
-
-@pytest.fixture
-def preset_dict_max_downloads_0(output_directory):
-    return {
-        "preset": "Jellyfin Music Videos",
-        "download": "https://youtube.com/watch?v=HKTNxEqsN3Q",
-        "format": "worst[ext=mp4]",
-        "overrides": {
-            "music_video_artist": "JMC",
-            "music_video_directory": output_directory,
-        },
-        "throttle_protection": {"max_downloads_per_subscription": {"max": 0}},
-    }
 
 
 @pytest.fixture
@@ -34,27 +18,6 @@ def preset_dict_subscription_download_proba_0(output_directory):
 
 
 class TestThrottleProtection:
-    def test_max_downloads(
-        self,
-        default_config,
-        preset_dict_max_downloads_0,
-        output_directory,
-    ):
-        single_video_subscription = Subscription.from_dict(
-            config=default_config,
-            preset_name="music_video_single_video_test",
-            preset_dict=preset_dict_max_downloads_0,
-        )
-
-        with assert_logs(
-            logger=throttle_protection_logger,
-            expected_message="Reached subscription max downloads of %d",
-            log_level="info",
-            expected_occurrences=1,
-        ):
-            transaction_log = single_video_subscription.download(dry_run=True)
-
-        assert transaction_log.is_empty
 
     def test_subscription_probability(
         self,
