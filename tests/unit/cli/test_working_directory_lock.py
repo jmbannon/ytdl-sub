c7b28d9aaca1416e2a3a5045199bc3cc0bf38ba9
Jesse Bannon <jbann1994@gmail.com>
2024-09-30 23:17:39 -0700

    integration actions

--- tests/unit/cli/test_working_directory_lock.py
+++ tests/unit/cli/test_working_directory_lock.py
@@ -9,28 +9,21 @@ from ytdl_sub.utils.file_lock import working_directory_lock
 from ytdl_sub.utils.system import IS_WINDOWS
 
 
-@pytest.fixture
-def config() -> ConfigFile:
-    return ConfigFile(
-        name="config", value={"configuration": {"working_directory": "test"}, "presets": {}}
-    )
-
-
-def test_working_directory_lock(config: ConfigFile):
+def test_working_directory_lock(default_config: ConfigFile):
     if IS_WINDOWS:
         return
 
     new_pid = os.fork()
     if new_pid == 0:  # is child
-        with working_directory_lock(config=config):
+        with working_directory_lock(config=default_config):
             time.sleep(3)
         return
 
     time.sleep(1)
     with pytest.raises(ValidationException, match="Cannot run two instances of ytdl-sub"):
-        with working_directory_lock(config=config):
+        with working_directory_lock(config=default_config):
             time.sleep(1)
 
     time.sleep(3)
-    with working_directory_lock(config=config):
+    with working_directory_lock(config=default_config):
         pass
