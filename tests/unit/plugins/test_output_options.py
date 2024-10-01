648201ed9cd43c0198cc20c208f8d1cdf92edd41
Jesse Bannon <jbann1994@gmail.com>
2024-09-30 23:18:33 -0700

    [DEV] OutputOptions unit tests (#1078)

--- tests/unit/plugins/test_output_options.py
+++ tests/unit/plugins/test_output_options.py
@@ -4,42 +4,46 @@ import pytest
 from expected_download import assert_expected_downloads
 from expected_transaction_log import assert_transaction_log_matches
 
-from ytdl_sub.entries.entry import ytdl_sub_chapters_from_comments
+from ytdl_sub.config.config_file import ConfigFile
 from ytdl_sub.subscriptions.subscription import Subscription
 
 
 @pytest.fixture
-def file_convert_subscription_dict(output_directory) -> Dict:
+def output_options_subscription_dict(output_directory) -> Dict:
     return {
-        "preset": "Jellyfin Music Videos",
-        "output_options": {"output_directory": output_directory},
-        "overrides": {
-            "music_video_artist": "JMC",
-            "url": "https://your.name.here",
+        "preset": [
+            "jellyfin_tv_show_by_date",
+            "season_by_year__episode_by_download_index",
+            "chunk_initial_download",
+        ],
+        "output_options": {
+            "output_directory": output_directory,
         },
-        "file_convert": {
-            "convert_to": "mkv",
-            "convert_with": "ffmpeg",
-            "ffmpeg_post_process_args": "-bitexact -vcodec copy -acodec copy -scodec mov_text",
+        "overrides": {
+            "tv_show_name": "JMC",
+            "url": "https://your.name.here",
         },
     }
 
 
-class TestFileConvert:
+class TestOutputOptions:
     @pytest.mark.parametrize("dry_run", [True, False])
-    def test_file_convert_custom_args(
+    def test_empty_info_json_and_thumb(
         self,
-        config,
-        subscription_name,
-        file_convert_subscription_dict,
-        output_directory,
+        config: ConfigFile,
+        subscription_name: str,
+        output_options_subscription_dict: Dict,
+        output_directory: str,
         mock_download_collection_entries,
         dry_run,
     ):
+        output_options_subscription_dict["output_options"]["thumbnail_name"] = ""
+        output_options_subscription_dict["output_options"]["info_json_name"] = ""
+
         subscription = Subscription.from_dict(
             config=config,
             preset_name=subscription_name,
-            preset_dict=file_convert_subscription_dict,
+            preset_dict=output_options_subscription_dict,
         )
 
         with mock_download_collection_entries(
@@ -53,10 +57,10 @@ class TestFileConvert:
         assert_transaction_log_matches(
             output_directory=output_directory,
             transaction_log=transaction_log,
-            transaction_log_summary_file_name=f"plugins/file_convert/custom_ffmpeg_args.txt",
+            transaction_log_summary_file_name="plugins/output_options/empty_info_json_thumb.txt",
         )
         assert_expected_downloads(
             output_directory=output_directory,
             dry_run=dry_run,
-            expected_download_summary_file_name="plugins/file_convert/custom_ffmpeg_args.json",
+            expected_download_summary_file_name="plugins/output_options/empty_info_json_thumb.json",
         )
