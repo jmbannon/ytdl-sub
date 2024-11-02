6260df805daad119bc8f5ce55e453f4a4693afd5
Jesse Bannon <jbann1994@gmail.com>
2024-11-02 00:07:41 -0700

    raw yaml repro

--- tests/e2e/youtube/test_debug_repro.py
+++ tests/e2e/youtube/test_debug_repro.py
@@ -1,3 +1,6 @@
+import json
+from typing import Dict
+
 import pytest
 import yaml
 from resources import file_fixture_path
@@ -5,29 +8,60 @@ from resources import file_fixture_path
 from ytdl_sub.subscriptions.subscription import Subscription
 
 
-@pytest.fixture
-def repro_preset_dict(output_directory):
-    with open(file_fixture_path("repro.yaml"), "r", encoding="utf-8") as yaml_file:
-        yaml_dict = yaml.safe_load(yaml_file)
-    out = yaml_dict["presets"]["subs"]
+def debug_yaml_to_preset_dict(debug_subscription_dict: Dict, output_directory: str) -> Dict:
+    out = debug_subscription_dict["presets"]["subs"]
     del out["preset"]
     out["output_options"]["output_directory"] = output_directory
     return out
 
 
+@pytest.fixture
+def debug_log_rerpo(output_directory):
+    with open(file_fixture_path("repro.yaml"), "r", encoding="utf-8") as yaml_file:
+        yaml_dict = yaml.safe_load(yaml_file)
+    return debug_yaml_to_preset_dict(
+        debug_subscription_dict=yaml_dict,
+        output_directory=output_directory,
+    )
+
+
+@pytest.fixture
+def subscription_yaml_preset(default_config, output_directory):
+    subscriptions = Subscription.from_file_path(
+        config=default_config, subscription_path=file_fixture_path("repro.yaml")
+    )
+    subscription_dict = json.loads(subscriptions[0].as_yaml())
+    return debug_yaml_to_preset_dict(
+        debug_subscription_dict=subscription_dict,
+        output_directory=output_directory,
+    )
+
+
 @pytest.mark.skipif(False, reason="Always skip repro, for local testing only")
 class TestReproduce:
-    def test_single_video_download(
+    def test_debug_log_repro(
         self,
         default_config,
         repro_preset_dict,
         output_directory,
     ):
-        single_video_subscription = Subscription.from_dict(
+        sub = Subscription.from_dict(
             config=default_config,
             preset_name="repro",
             preset_dict=repro_preset_dict,
         )
 
-        transaction_log = single_video_subscription.download(dry_run=False)
+        transaction_log = sub.download(dry_run=False)
+        assert not transaction_log.is_empty
+
+    def test_subscription_yaml_repro(
+        self, default_config, subscription_yaml_preset, output_directory
+    ):
+        sub = Subscription.from_dict(
+            config=default_config,
+            preset_name="repro",
+            preset_dict=subscription_yaml_preset,
+        )
+
+        transaction_log = sub.download(dry_run=False)
         assert not transaction_log.is_empty
