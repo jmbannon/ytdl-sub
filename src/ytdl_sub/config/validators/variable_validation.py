80e383465dddd7e20c20115aa24f2ed651b8c6f1
Jesse Bannon <jbann1994@gmail.com>
2026-01-03 12:26:47 -0800

    with variables

--- src/ytdl_sub/config/validators/variable_validation.py
+++ src/ytdl_sub/config/validators/variable_validation.py
@@ -7,7 +7,7 @@ from ytdl_sub.config.plugin.preset_plugins import PresetPlugins
 from ytdl_sub.config.preset_options import OutputOptions
 from ytdl_sub.config.validators.options import OptionsValidator
 from ytdl_sub.downloaders.url.validators import MultiUrlValidator
-from ytdl_sub.validators.string_formatter_validators import validate_formatters
+from ytdl_sub.validators.string_formatter_validators import validate_formatters, _validate_formatter
 
 
 class VariableValidation:
