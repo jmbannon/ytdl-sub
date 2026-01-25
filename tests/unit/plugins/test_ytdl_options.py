f820a61ec876384efdc26c31e21e527275becdfe
Jesse Bannon <jbann1994@gmail.com>
2026-01-25 08:56:47 -0800

    [BACKEND] More tests around passed-in ytdl-options

--- tests/unit/plugins/test_ytdl_options.py
+++ tests/unit/plugins/test_ytdl_options.py
@@ -3,11 +3,14 @@ from typing import Any
 from typing import Dict
 
 import pytest
-from conftest import get_match_filters
+import yt_dlp
+from conftest import assert_logs
 
 from ytdl_sub.config.config_file import ConfigFile
+from ytdl_sub.downloaders.ytdlp import YTDLP
 from ytdl_sub.subscriptions.subscription import Subscription
 from ytdl_sub.utils.exceptions import ValidationException
+from ytdl_sub.utils.ffmpeg import FFMPEG
 
 
 @pytest.fixture
@@ -19,6 +22,40 @@ def preset_dict(output_directory) -> Dict[str, Any]:
 
 
 class TestYtdlOptions:
+
+    def test_ytdl_options_are_strings(
+        self,
+        default_config: ConfigFile,
+        preset_dict: Dict[str, Any],
+        working_directory,
+    ):
+        expected_ytdl_options = {
+            "ignoreerrors": True,
+            "outtmpl": f"{working_directory}/test_ytdl_options/%(id)S.%(ext)s",
+            "writethumbnail": False,
+            "ffmpeg_location": FFMPEG.ffmpeg_path(),
+            "match_filter": yt_dlp.utils.match_filter_func(
+                ["!is_live & !is_upcoming & !post_live"], []
+            ),
+            "skip_download": True,
+            "writeinfojson": True,
+            "extract_flat": "discard",
+        }
+
+        with (
+            assert_logs(
+                logger=YTDLP.logger,
+                expected_message=f"ytdl_options: {str(expected_ytdl_options)}",
+                log_level="debug",
+                expected_occurrences=1,
+            ),
+        ):
+            _ = Subscription.from_dict(
+                config=default_config,
+                preset_name="test_ytdl_options",
+                preset_dict=preset_dict,
+            ).download(dry_run=True)
+
     def test_cookiefile_does_not_exist(
         self,
         default_config: ConfigFile,
